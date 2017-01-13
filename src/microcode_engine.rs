f3eaa5788b625d753f0664197ec468aadb3935b2
Matthew Bettcher <mattbettcher@gmail.com>
2017-01-13 10:59:50 -0500

    Set up how to store instruction definitions. Idea is that we have a HashMap<u8, Vec<MicroCode>> and that stores the MicroCode for each opcode of the 6502. Still need to change how instructions are 'decoded' and handled in Mos6502.

--- src/microcode_engine.rs
+++ src/microcode_engine.rs
@@ -2,8 +2,8 @@
     Defines the microcode architecture used to implement the 6502.
 */
 
-#[derive(Debug)]
-pub enum MicroOperation {
+#[derive(Debug, Clone)]
+pub enum MicroCode {
     Fetch,
     FetchNext,
     NextCycle,
@@ -26,11 +26,38 @@ pub struct MicrocodeEngine {
     pub sub_step: usize,
 }
 
+use std::collections::HashMap;
+
 #[derive(Debug)]
-pub struct InstructionDef { pub ops: Vec<MicroOperation> }
+pub struct InstructionDef { 
+    pub ops: Vec<MicroCode>,
+    
+    pub instructions: Option<HashMap<u8, Vec<MicroCode>>>,
+}
 
 impl From<u8> for InstructionDef {
+    
     fn from(op:u8) -> Self {
+        use self::MicroCode::*;
+
+        const IMM: [MicroCode; 2] = [Fetch, NextCycle];
+
+        const CLC: [MicroCode; 2] = [Clear(1), NextCycle];
+
+        let mut instructions = HashMap::new();
+
+        // this can be a macro
+        instructions.insert(0x18, concatenate_arrays::<MicroCode>(&IMM, &CLC));
+
         unimplemented!()
     }
+}
+
+fn concatenate_arrays<T: Clone>(x: &[T], y: &[T]) -> Vec<T> {
+    let mut concat: Vec<T> = vec![x[0].clone(); x.len()];
+ 
+    concat.clone_from_slice(x);
+    concat.extend_from_slice(y);
+ 
+    concat
 }
